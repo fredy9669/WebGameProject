df8575936732ef2fa7af90806d3136e53fb8bbcc
fredy9669 <ferdinand.havlik@gmail.com>
2022-04-13 23:19:53 +0200

    Menu update

--- quizJS.js
+++ quizJS.js
@@ -2,12 +2,16 @@ var statusMenu = false;
 
 function showMenu(){
 	if(statusMenu == true){
+		document.getElementById('mainID').style.opacity = "1";
+		document.getElementById('headerID').style.opacity = "1";
 		document.getElementById('drawer').style.width = "60px";
 		document.getElementById('menuItemsID').style.display = "none";
-		statusMenu = false;
-		hideSubmenu();
+		statusMenu = false;		
+		hideSubmenu();		
 		return;
 	}else if(statusMenu == false){
+		document.getElementById('mainID').style.opacity = "0.2";
+		document.getElementById('headerID').style.opacity = "0.2";
 		document.getElementById('drawer').style.width = "200px";
 		document.getElementById('menuItemsID').style.display = "inline";
 		statusMenu = true;
