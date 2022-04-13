0f982f8e1f70457c6993a25abf67db634f80cf28
fredy9669 <ferdinand.havlik@gmail.com>
2022-04-13 22:06:39 +0200

    First version

---
+++ quizJS.js
@@ -0,0 +1,24 @@
+var statusMenu = false;
+
+function showMenu(){
+	if(statusMenu == true){
+		document.getElementById('drawer').style.width = "60px";
+		document.getElementById('menuItemsID').style.display = "none";
+		statusMenu = false;
+		hideSubmenu();
+		return;
+	}else if(statusMenu == false){
+		document.getElementById('drawer').style.width = "200px";
+		document.getElementById('menuItemsID').style.display = "inline";
+		statusMenu = true;
+		return;
+	}	
+}	
+
+function showSubmenu(){
+	document.getElementById('submenuID').style.width = "200px";
+}
+
+function hideSubmenu(){
+	document.getElementById('submenuID').style.width = "0px";
+}
